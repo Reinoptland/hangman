6955122289def7ec7726ab575ab21200f9bfafcc
Reinoptland <reinoptland@gmail.com>
2021-01-28 15:52:12 +0100

    Write todo list for displayWordSoFar

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,14 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  // - [ ] zet je testje op test.only (later weer weghalen)
+  // - [ ] loggen: wat is word, wat is guesses
+  // - [ ] javascript, ['a', 'b'] -> "_ _ _ _ _ _ _ _ _ _ "
+  // - [ ] javascript "splitten" split() -> ['j','a','v','a','s','c','r','i','p','t']
+  // - [ ] een loop maken -> ['j','a','v','a','s','c','r','i','p','t']
+  // - [ ] checken: zit de letter in guesses?
+  // - [ ] if else
+  // - [ ] zo nee: "_ "
+  // - [ ] zo ja: "j " (niet j, maar de letter die we dan checken)
+  // - [ ] klaar? -> onze string die we hebben gemaakt returnen
 }
 
 function isGameWon(word, guesses) {
