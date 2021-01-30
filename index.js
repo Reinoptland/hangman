09f41041b6861c5ded893ce6154077c2d8e8834a
Reinoptland <reinoptland@gmail.com>
2021-01-30 15:18:29 +0100

    Draw gallows and add to main program

--- index.js
+++ index.js
@@ -5,29 +5,15 @@ const {
   isGameLost,
   countMistakes,
 } = require("./gamelogic");
+const drawGallows = require("./gallows");
 
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
-  console.log(`Je mag nog ${7 - countMistakes(word, guesses)} fouten maken`);
   const wordSoFar = displayWordSoFar(word, guesses);
   console.log(wordSoFar);
 
-  const gameWon = isGameWon(word, guesses);
-  if (gameWon) {
-    console.log("YOU WON!");
-    // returning to make the game function stop running
-    return;
-  }
-
-  const gameLost = isGameLost(word, guesses);
-  if (gameLost) {
-    console.log("YOU HAVE DIED...");
-    // returning to make the game function stop running
-    return;
-  }
-
-  // using keyIn to get on letter,
-  // limit to limit the options of the user
+  // using keyIn to get one letter,
+  // using limit to limit the options of the user
   // caseSensitive to only get lowercase letters
   // check readline-sync docs for details
   const letter = keyIn("Raad een letter: ", {
@@ -37,6 +23,24 @@ function game(word, guesses) {
 
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
+
+  const gameWon = isGameWon(word, guesses);
+  if (gameWon) {
+    console.log("YOU WON!");
+    // returning to make the game function stop running
+    return;
+  }
+
+  const mistakeCount = countMistakes(word, guesses);
+  console.log(`Je mag nog ${7 - mistakeCount} fouten maken`);
+  drawGallows(mistakeCount);
+
+  const gameLost = isGameLost(word, guesses);
+  if (gameLost) {
+    console.log("YOU HAVE DIED...");
+    // returning to make the game function stop running
+    return;
+  }
 
   // volgende ronde! we roepen game nog een keer aan
   game(word, guesses);
