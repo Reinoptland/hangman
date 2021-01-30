0c7a871d1ee3f24066b1b9ce6d3e575a0a0d3582
Reinoptland <reinoptland@gmail.com>
2021-01-30 09:48:30 +0100

    Increment mistakeCount with 1 when letter is not guessed

--- gamelogic.js
+++ gamelogic.js
@@ -34,12 +34,15 @@ function isGameWon(word, guesses) {
 function isGameLost(word, guesses) {
   console.log("INPUT", "WORD:", word, "GUESSES", guesses);
   let mistakeCount = 0;
-  console.log("MISTAKE COUNT:", mistakeCount);
   for (let index = 0; index < guesses.length; index++) {
     const guess = guesses[index];
     console.log("ONE GUESS IN LOOP:", guess);
     const isGuessCorrect = word.includes(guess);
     console.log("GUESS CORRECT?", word, guess, isGuessCorrect);
+    if (!isGuessCorrect) {
+      mistakeCount = mistakeCount + 1;
+    }
+    console.log("MISTAKE COUNT:", mistakeCount);
   }
 }
 
